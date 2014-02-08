cbc846a9adecfd3a3b8cf0528c564985707160e1
Markus Wolf <mail@markus-wolf.de>
2014-02-08 08:33:41 +0100

    Exposing the url stream-handler

---
+++ src/main/java/de/matrixweb/vfs/VFSURLStreamHandler.java
@@ -0,0 +1,11 @@
+package de.matrixweb.vfs;
+
+/**
+ * @author markusw
+ */
+public class VFSURLStreamHandler extends
+    de.matrixweb.vfs.internal.VFSManager.VFSURLStreamHandler {
+
+  // Just to expose the URLStreamHandler
+
+}
